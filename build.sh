56445d75640246be9969f8a8e7a887a2f9279b88
Kenneth Zhao <kenneth.zhao@gmail.com>
2019-11-12 15:28:54 -0800

    fixing build script to reflect the file reorg

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-docker build --compress --rm --no-cache -t "epenguincom/flask:3.7-alpine" .
+docker build --compress --rm --no-cache -t "epenguincom/flask:3.7-alpine" . -f 3.7-alpine/Dockerfile
